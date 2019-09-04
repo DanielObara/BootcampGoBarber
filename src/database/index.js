2af7709c830087dcad2bb59ba0f5ed196605ae16
daniel.obara <daniel.tsutomu@hotmail.com>
2019-09-03 23:17:59 -0300

    INITIAL PROJECT, ALL PATH/FOLDERS STRUCTURE AND DEPENDENCIES ITS OK

---
+++ src/database/index.js
@@ -0,0 +1,38 @@
+import Sequelize from 'sequelize';
+
+// Import dos models
+import User from '../app/models/User';
+
+// Import das configurações da DB
+import databaseConfig from '../config/database';
+
+// Array com os models da aplicação
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  // Responsável por fazer a conexão com o banco e carregar os models
+  init() {
+    // Passando as configurações à connection
+    this.connection = new Sequelize(databaseConfig);
+
+    // Percorrendo as models e passando a connection
+    models
+      .map(model => model.init(this.connection))
+      // Percorrendo os models novamente e só vai associar se o model.associate existir
+      // Pois no model file não existe o associate.
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  // mongo() {
+  //   this.mongoConnection = mongoose.connect(
+  //     'mongodb://localhost:27017/meetapp',
+  //     { useNewUrlParser: true, useFindAndModify: true }
+  //   );
+  // }
+}
+
+export default new Database();
