7a9ac6d8d809da432e6629a626344ed7bb7b3ec0
daniel.obara <daniel.tsutomu@hotmail.com>
2019-09-04 23:13:22 -0300

    User, session, auth and file

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,13 @@
 import { Router } from 'express';
+// Controllers
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ msg: 'primeiro teste' }));
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.get('/users', UserController.index);
 
 export default routes;
